34e74ef0fc7489ba1badc3b610deead1a26de94b
ofriedma <or@speedb.io>
2023-09-06 13:32:10 +0300

    works

--- db/db_impl/db_impl.cc
+++ db/db_impl/db_impl.cc
@@ -1120,11 +1120,11 @@ void DBImpl::DumpStats() {
       ROCKS_LOG_INFO(immutable_db_options_.info_log, "--- Malloc STATS %s",
                      stats.c_str());
     }
-    ROCKS_LOG_DEBUG(immutable_db_options_.info_log,
+    ROCKS_LOG_INFO(immutable_db_options_.info_log,
                     "Arena Stats: Arena total: %" PRIu64,
                     Arena::arena_tracker_.total.load());
     for (const auto& it : Arena::arena_tracker_.arena_stats) {
-      ROCKS_LOG_DEBUG(immutable_db_options_.info_log, "%s : %" PRIu64,
+      ROCKS_LOG_INFO(immutable_db_options_.info_log, "%s : %" PRIu64,
                       it.first.c_str(), it.second.load());
     }
     ROCKS_LOG_INFO(immutable_db_options_.info_log, "CF stats: ");
@@ -1134,17 +1134,17 @@ void DBImpl::DumpStats() {
         std::string cf_name = cfd->GetName();
         uint64_t allocated_cf = cfd->mem()->ApproximateMemoryUsageFast() + cfd->imm()->ApproximateMemoryUsage();
         cfs_total_memory += allocated_cf;
-        ROCKS_LOG_DEBUG(immutable_db_options_.info_log, "CF Name: %s , memory used: %" PRIu64, cf_name.c_str() ,allocated_cf);
+        ROCKS_LOG_INFO(immutable_db_options_.info_log, "CF Name: %s , memory used: %" PRIu64, cf_name.c_str() ,allocated_cf);
       }
     }
-    ROCKS_LOG_DEBUG(immutable_db_options_.info_log, "Total CF memory usage: %" PRIu64, cfs_total_memory);
+    ROCKS_LOG_INFO(immutable_db_options_.info_log, "Total CF memory usage: %" PRIu64, cfs_total_memory);
     std::string out;
     this->GetProperty("rocksdb.block-cache-usage", &out);
-    ROCKS_LOG_DEBUG(immutable_db_options_.info_log, "rocksdb.block-cache-usage: %s", out.c_str());
+    ROCKS_LOG_INFO(immutable_db_options_.info_log, "rocksdb.block-cache-usage: %s", out.c_str());
     this->GetProperty("rocksdb.estimate-table-readers-mem", &out);
-    ROCKS_LOG_DEBUG(immutable_db_options_.info_log, "rocksdb.estimate-table-readers-mem: %s", out.c_str());
+    ROCKS_LOG_INFO(immutable_db_options_.info_log, "rocksdb.estimate-table-readers-mem: %s", out.c_str());
     this->GetProperty("rocksdb.block-cache-pinned-usage", &out);
-    ROCKS_LOG_DEBUG(immutable_db_options_.info_log, "rocksdb.block-cache-pinned-usage: %s", out.c_str());
+    ROCKS_LOG_INFO(immutable_db_options_.info_log, "rocksdb.block-cache-pinned-usage: %s", out.c_str());
   }
 
   PrintStatistics();
